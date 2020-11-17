150b1e5ad2844b17aec027c8c1450c2a1ea66462
admin <admin@YD03-07>
2020-11-18 08:56:46 +0900

    과제

--- variable/src/variable/arrayExample1.java
+++ variable/src/variable/arrayExample1.java
@@ -1,7 +1,7 @@
 package variable;
 
 public class arrayExample1 {
-//1,3,5,7,9 (홀수 위치에만 있는 값만 합하는 프로그램)
+/*1,3,5,7,9 (홀수 위치에만 있는 값만 합하는 프로그램)*/
 	public static void main(String[] args) {
 		int[] numbers = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
 		int sum = 0;
